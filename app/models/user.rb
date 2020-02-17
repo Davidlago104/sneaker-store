6f4a433966fab7b734c063c45975beafda4b9f92
David <davidlago104@gmail.com>
2020-02-17 15:22:21 -0500

    Added sign up form, and have show the user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :reviews
   has_many :sneakers, through: :reviews
+  has_secure_password #gives us authenticate method, validation for password is true
 end
