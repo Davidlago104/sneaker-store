53b5a9680beb6a0901652f8d60ce7c5ff3782ced
David <davidlago104@gmail.com>
2020-02-20 18:05:57 -0500

    Added new sneakers form and with brand

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,5 +1,23 @@
 class SneakersController < ApplicationController
 
-  def new  
+  def new
+    @sneaker = Sneaker.new
+    @sneaker.build_brand
+  end
+
+  def create
+    @sneaker = Sneaker.new(sneaker_params)
+    @sneaker.user_id = session[:user_id]
+    if @sneaker.save
+      redirect_to sneaker_path(@sneaker)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sneaker_params
+    params.require(:sneaker).permit(:name, :price, :descrpition, :brand_id, brand_attributes: [:name])
   end
 end
--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -4,4 +4,6 @@ class Sneaker < ApplicationRecord
 
   has_many :reviews
   has_many :users, through: :reviews
+
+  accepts_nested_attributes_for :brand
 end
