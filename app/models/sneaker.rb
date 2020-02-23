53ae166d3928817ec3a8f61162e0aaa136eb46c2
David <davidlago104@gmail.com>
2020-02-23 16:54:47 -0500

    Made my own validation for price

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,9 +1,20 @@
 class Sneaker < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   has_many :reviews
   has_many :users, through: :reviews
 
 
   validates :name, presence: true
+  validates :name, length: {minimum: 3}
+  validates :name, uniqueness: true
+
+  validate :price_range
+
+  def price_range #an attempt to give a price cap to price
+    if self.price > 500
+      errors.add(:price, "$500!? No shoe is worth that amount.")
+    end
+  end
+
 end
