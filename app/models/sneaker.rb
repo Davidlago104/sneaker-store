91504b9d411be7619e1e6d8e94685ab29681e3b1
David <davidlago104@gmail.com>
2020-02-24 15:41:10 -0500

    Added pricing scopes to sneaker

--- app/models/sneaker.rb
+++ app/models/sneaker.rb
@@ -1,16 +1,21 @@
 class Sneaker < ApplicationRecord
   belongs_to :user, optional: true
-
   has_many :reviews
   has_many :users, through: :reviews
 
-
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true
+  # validates :name, presence: { scope: :review, message}
   validates :name, length: {minimum: 3}
-  
+
   validates :description, presence: true
 
   validate :price_range
+
+
+  scope :pricing, -> { where("price < 500") }
+  scope :over_price, -> { where("price > 500") }
+
+  scope :jordan_1, -> { where("name = jordan 1") }
 
   def price_range #an attempt to give a price cap to price
     if self.price > 500
