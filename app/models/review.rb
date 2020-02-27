d1d8334b5b383fd1fc6c123a08e20006394f5cf6
David <davidlago104@gmail.com>
2020-02-27 16:18:34 -0500

    added validations to stars

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ApplicationRecord
   belongs_to :sneakers, optional: true
 
   validates :stars, presence: true
+  validates :stars, numericality: { greater_than: 0}
+  validates :stars, numericality: { less_than: 5}
   validates :title, presence: true
   validates :thoughts, presence: true
 
