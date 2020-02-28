0b77e86c34e57f4b9c8934ddc0c6d5aa5f2ea5c5
David <davidlago104@gmail.com>
2020-02-28 14:16:09 -0500

    added active Storage for images

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
 
   validates :stars, presence: true
   validates :stars, numericality: { greater_than: 0}
-  validates :stars, numericality: { less_than: 5}
+  validates :stars, numericality: { less_than: 6}
   validates :title, presence: true
   validates :thoughts, presence: true
 
