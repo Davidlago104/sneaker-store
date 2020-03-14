641b1239d43503d152ae4b0e560547af8137736c
David <davidlago104@gmail.com>
2020-03-14 00:02:06 -0400

    fixed reviews to where the sneaker_id is no longer nil

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,22 +9,22 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def create
+    @review = current_user.reviews.build(review_params)
+
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
   def index
     if params[:sneaker_id] #nested
       @sneakers = Sneaker.find_by_id(params[:sneaker_id])
       @reviews = @sneakers.reviews
     else
       @reviews = Review.all #everything else
-    end
-  end
-
-  def create
-    @review = current_user.reviews.build(review_params)
-
-    if logged_in? && @review.save
-      redirect_to review_path(@review)
-    else
-      render :new
     end
   end
 
